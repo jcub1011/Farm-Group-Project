2a54cbcd58030095f1709dd12f08be4fd01843b5
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2023-11-06 15:47:39 -0500

    Working on building entity class.

---
+++ VisualizationItems/BuildingEntity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Farm_Group_Project.VisualizationItems
+{
+    public class BuildingEntity
+    {
+        #region Properties
+        public string Name { private set; get; }
+        private readonly ObservableCollection<Control> _children;
+        public ObservableCollection<Control> Children { 
+            get
+            {
+                return _children;
+            } 
+        }
+        #endregion
+
+        #region Methods
+        private void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch(e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    break;
+                default:
+                    break;
+            }
+        }
+        #endregion
+
+        #region Constuctors
+        public BuildingEntity()
+        {
+            Name = "";
+            _children = new();
+            _children.CollectionChanged += OnCollectionChanged;
+        }
+
+        ~BuildingEntity()
+        {
+            _children.CollectionChanged -= OnCollectionChanged;
+        }
+        #endregion
+    }
+}
