7de2a5cc5b1669def9080b849c317641c8853d5b
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2023-11-08 11:19:02 -0500

    Added interfaces for items and item containers.

---
+++ VisualizationItems/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Farm_Group_Project.VisualizationItems
+{
+    public interface IItem
+    {
+        public string Name { get; set; }
+        public int[] Location { get; set; }
+        public int[] Dimensions { get; set; }
+        public double Price { get; set; }
+    }
+
+    public interface IItemContainer: IItem
+    {
+        public void AddItem(IItem item);
+        public void AddItemContainer(IItemContainer container);
+        public List<object> Children { get; }
+    }
+}
