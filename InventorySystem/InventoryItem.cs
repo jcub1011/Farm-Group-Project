51410a78283975e26a1837ef7667b763208935eb
Jacob <me@here.com>
2023-11-11 09:42:06 -0500

    Implemented property changed interface for InventoryItem.

--- InventorySystem/InventoryItem.cs
+++ InventorySystem/InventoryItem.cs
@@ -1,29 +1,94 @@
 ﻿using Farm_Group_Project.VisualizationItems;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Farm_Group_Project.InventorySystem
 {
-    public class InventoryItem
+    public class InventoryItem: INotifyPropertyChanged
     {
-        public string ItemName { get; set; }
-        public string ItemTag { get; set; }
-        public double[] Location { get; set; }
-        public double[] Dimensions { get; set; }
-        public double Price { get; set; }
-        public List<InventoryItem> Children { get; set; }
+        string _itemName;
+        public string ItemName
+        {
+            get => _itemName;
+            set
+            {
+                _itemName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemName)));
+            }
+        }
 
-        public InventoryItem(string itemName, string itemTag, double[] location, double[] dims, double price, List<InventoryItem>? children = null)
+        string _itemTag;
+        public string ItemTag
+        {
+            get => _itemTag;
+            set
+            {
+                _itemTag = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemTag)));
+            }
+        }
+
+        double[] _location;
+        public double[] Location
+        {
+            get => _location;
+            set
+            {
+                if (value.Length != 2) throw new ArgumentException("Location must be an array of length 2 of format { x, y }.");
+                _location = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Location)));
+            }
+        }
+
+        double[] _dimensions;
+        public double[] Dimensions
+        {
+            get => _dimensions;
+            set
+            {
+                if (value.Length != 2) throw new ArgumentException("Dimension must be an array of length 2 of format { width, height }.");
+                _dimensions = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Dimensions)));
+            }
+        }
+
+        double _price;
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Price)));
+            }
+        }
+
+        ObservableCollection<InventoryItem>? _children;
+        public ObservableCollection<InventoryItem>? Children
+        {
+            get => _children;
+            set
+            {
+                _children = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Children)));
+            }
+        }
+
+        public InventoryItem(string itemName, string itemTag, double[] location, double[] dims, double price, ObservableCollection<InventoryItem>? children = null)
         {
             ItemName = itemName;
             ItemTag = itemTag;
             Location = location;
             Dimensions = dims;
             Price = price;
-            Children = children ?? new List<InventoryItem>();
+            Children = children ?? new();
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
